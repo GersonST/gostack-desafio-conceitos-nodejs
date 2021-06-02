c294148a815f5c70a552c35413fd3ab4d56318d4
GersonST <gersonsevero90@gmail.com>
2021-06-02 18:35:15 -0300

    Commit inicial

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -12,18 +12,73 @@ const repositories = [];
 
 app.get("/repositories", (request, response) => {
   // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
   // TODO
+  const {title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+  }
+
+  const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+
+  if (findRepositoryIndex === -1) {
+    return response.status(400).json ({error: 'Repository does not exists.'});
+  }
+
+
+
+
+
+
+
+  repositories[findRepositoryIndex] = repository
+
+  return response.json(repository);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (findRepositoryIndex >= 0){
+    repositories.splice(findRepositoryIndex, 1);
+  } else {
+    return response.status(400).json({error: 'Repository does not exists.'});
+  }
+
+
+  return response.status(204).send();
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
